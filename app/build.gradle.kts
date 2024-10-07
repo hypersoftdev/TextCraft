82e21943bf70017b8eccfce73e97082b14c18001
Hypersoft Inc <social.yaqoob@gmail.com>
2024-10-07 11:24:40 +0500

    TextCraft

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.hypersoft.textcraft"
         minSdk = 24
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 135
+        versionName = "1.3.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
