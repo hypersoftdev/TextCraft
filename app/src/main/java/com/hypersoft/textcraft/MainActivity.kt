a3d8f191881426e9abb9b280e3e5c21e1f880a71
Dev8HS <Dev@hypersoft.com.pk>
2024-10-18 16:51:43 +0500

    Readme Updated

--- app/src/main/java/com/hypersoft/textcraft/MainActivity.kt
+++ app/src/main/java/com/hypersoft/textcraft/MainActivity.kt
@@ -1,17 +1,10 @@
-package com.hypersoft.craftzone
+package com.hypersoft.textcraft
 
-import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.google.android.material.textview.MaterialTextView
-
-import com.hypersoft.textcraft.TextCraft
-import com.hypersoft.textcraft.extensions.Extension.shadeTextColor
-import com.hypersoft.textcraft.extensions.Extension.strikeThroughTextLocalized
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
